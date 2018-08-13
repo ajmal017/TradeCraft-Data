8d63d17a3a59814bed393d421632266d6d3015c8
rajavaraparla <varaparla.raja@gmail.com>
2018-08-13 16:23:37 +0530

    Initial Commit

---
+++ utils/extract-utils.py
@@ -0,0 +1,18 @@
+'''
+    Utilities related extract historical ieod data.
+'''
+
+import datetime
+
+def login_histdata_system(api_key,api_secret,redirect_uri):
+    """
+    :param api_key:
+    :type string:
+    :param api_secret:
+    :type string:
+    :param redirect_uri:
+    :type string:
+    :return:
+    :rtype:
+    """
+    pass
